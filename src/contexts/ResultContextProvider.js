13f0c3ce3d1cc66535fd5ca85b62e660a6187170
ABHINAV NARWAL <72136278+narwalabhinav@users.noreply.github.com>
2023-09-26 22:44:58 +0530

    Initial Commit

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const ResultContext = createContext();
+const baseUrl = 'https://google-search74.p.rapidapi.com'
+
+export const ResultContextProvider = ({ children }) => {
+    const [results, setResults] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+  
+    const getResults = async (type) => {
+      setIsLoading(true);
+  
+      const res = await fetch(`${baseUrl}${type}`, {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+            'X-RapidAPI-Host': 'google-search74.p.rapidapi.com'
+        },
+      });
+  
+      const data = await res.json();
+
+      console.log(data);
+  
+      setResults(data);
+      setIsLoading(false);
+    };
+  
+    return (
+      <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}>
+        {children}
+      </ResultContext.Provider>
+    );
+};
+  
+export const useResultContext = () => useContext(ResultContext);
