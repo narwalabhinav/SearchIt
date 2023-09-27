fd347e7c69dcd00f67d0b94abc05fd007819718a
ABHINAV NARWAL <72136278+narwalabhinav@users.noreply.github.com>
2023-09-27 12:42:01 +0530

    Second Commit

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -20,8 +20,6 @@ export const ResultContextProvider = ({ children }) => {
       });
   
       const data = await res.json();
-
-      console.log(data);
   
       setResults(data);
       setIsLoading(false);
